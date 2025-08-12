ccd45a2a03afc5cb9d06558ea64106318b4e6666
The-Berin <barongartner@gmail.com>
2025-08-12 16:39:53 -0600

    ReType Ver 1.0.py - Comments

--- Ver 1.0/ReType Ver 1.0.py
+++ Ver 1.0/ReType Ver 1.0.py
@@ -4,9 +4,9 @@ import time
 import random
 import math
 
-index = 0
+index = 0 # Position of cursor during typing
 
-ShortPause = 50
+ShortPause = 50 # Time in miliseconds between character types
 
 RandomErrors = ['jj', 'gh', 'ik', 'il', 'l;']
 
@@ -17,9 +17,9 @@ typo = random.choice(RandomErrors)
 # Def Section
 
 def quit():
-    root.destroy()
+    root.destroy() # Quits App
 
-def StartWriting():
+def StartWriting(): # Sets ReadyText variable and prints it to console
     global index
     global ReadyText
     ReadyText = TextEntry.get("1.0",tk.END).strip()
@@ -27,38 +27,38 @@ def StartWriting():
     index = 0
     root.after(3000, TypeNextChar)
 
-def ClrText():
-    TextEntry.delete(1.0, tk.END)
+def ClrText(): # Clears TextBox does NOT clear variable
+    TextEntry.delete(1.0, tk.END) 
 
-def StopChance():
+def StopChance(): # Randomly Chooses if a pause will occur, loop
     global stopchance
     stopchance = random.randint(1,10)
     print(f"Stop Chance: {stopchance}")
     root.after(1000, StopChance)
 
-def ErrorChance():
+def ErrorChance(): # Randomly chooses if an error will occur, loop
     global errorchance
     errorchance = random.randint(1,100)
     print(f"Error Chance: {errorchance}")
     root.after(1000, ErrorChance)
 
 
-def TypeNextChar():
+def TypeNextChar(): # Types characters one at a time while checking if its time for a pause/ error
     global ShortPause
     global stopchance
     global errorchance
     global index
     global typo
     ErrorChance()
-    if index < len(ReadyText):
+    if index < len(ReadyText): # Checks if there are any letters left to type
         pyautogui.typewrite(ReadyText[index])
         index += 1
         root.after(ShortPause, TypeNextChar)
     if stopchance == 10:
-         ShortPause = 3000
+         ShortPause = 3000 # Long pause between keystrokes
     else: ShortPause = 50
 
-    if errorchance == 100:
+    if errorchance == 100: # Makes a random mistake and goes back and fixes it
         pyautogui.typewrite(typo, 0.2)
         time.sleep(1)
         pyautogui.press('backspace')
